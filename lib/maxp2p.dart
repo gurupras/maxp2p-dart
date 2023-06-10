e109144527e2f7f3432db15609ad9f3a36358dab
Guru Prasad Srinivasa <gurupras@buffalo.edu>
2023-06-10 04:46:48 -0400

    Add MaxP2P implementation

--- lib/maxp2p.dart
+++ lib/maxp2p.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:maxp2p/blocking_queue.dart';
 import 'package:maxp2p/chunk.dart';
 import 'package:maxp2p/chunk_manager.dart';
 import 'package:maxp2p/serialize.dart';
@@ -12,6 +13,7 @@ import 'package:maxp2p/web_socket_signaler.dart';
 import 'package:mutex/mutex.dart';
 
 typedef MessageCallback = Future<void> Function(Uint8List bytes);
+typedef Callback<T> = FutureOr<T> Function();
 
 const maxChunkSize = 16384 - (80); // Approximate size of metadata of Chunk
 
@@ -20,6 +22,7 @@ class PeerConnection {
   final Mutex mutex;
   final int maxBufferedAmount;
   RTCDataChannel? dc;
+  Callback<void>? onClose;
   Completer<void>?
       completer; // TODO: Ideally this should be a map<dc, completer>
 
@@ -47,7 +50,9 @@ class PeerConnection {
         //     'Sent ${chunkBytes.length} bytes. buffered=$bufferedAmount max=$maxBufferedAmount');
         if (bufferedAmount > maxBufferedAmount) {
           completer = Completer();
+          // print('DC flow-control triggered');
           await completer!.future;
+          // print('DC available again');
           completer = null;
         }
       });
@@ -71,13 +76,23 @@ class PeerConnection {
       onMessage(bytes);
     };
   }
+
+  Future<void> close() async {
+    if (dc != null) {
+      await dc!.close();
+    }
+    await pc.close();
+    if (onClose != null) {
+      await onClose!();
+    }
+  }
 }
 
 class WebSocketBasedP2P {
   final String deviceID;
   final WebSocketSignaler signaler;
   final Mutex mutex;
-  late Map<String, Map<String, PeerConnection>> connections;
+  late Map<String, Map<String, PeerConnection>> connectionsMap;
   final ChunkManager chunkManager;
   MessageCallback onMessage;
 
@@ -85,7 +100,7 @@ class WebSocketBasedP2P {
       {required this.deviceID, required this.signaler, required this.onMessage})
       : mutex = Mutex(),
         chunkManager = ChunkManager(onPacket: onMessage) {
-    connections = <String, Map<String, PeerConnection>>{};
+    connectionsMap = <String, Map<String, PeerConnection>>{};
 
     signaler.onICECandidate = (src, connectionID, candidate) {
       final conn = getPC(src, connectionID);
@@ -117,10 +132,10 @@ class WebSocketBasedP2P {
   }
 
   PeerConnection getPC(String peer, String connectionID) {
-    if (!this.connections.containsKey(peer)) {
+    if (!this.connectionsMap.containsKey(peer)) {
       throw 'No connection from peer $peer';
     }
-    final connections = this.connections[peer]!;
+    final connections = this.connectionsMap[peer]!;
     if (!connections.containsKey(connectionID)) {
       throw 'No connection with id $connectionID';
     }
@@ -133,10 +148,10 @@ class WebSocketBasedP2P {
       signaler.sendICECandidate(peer, connectionID, candidate);
     };
 
-    if (!connections.containsKey(peer)) {
-      connections[peer] = <String, PeerConnection>{};
+    if (!connectionsMap.containsKey(peer)) {
+      connectionsMap[peer] = <String, PeerConnection>{};
     }
-    final peerConnections = connections[peer]!;
+    final peerConnections = connectionsMap[peer]!;
     peerConnections[connectionID] = conn;
   }
 
@@ -157,6 +172,12 @@ class WebSocketBasedP2P {
 
     final dc = await pc.createDataChannel("dc", dcProps);
     final conn = PeerConnection(pc: pc);
+    conn.onClose = () {
+      connectionsMap[peer]!.remove(connectionID);
+      if (connectionsMap[peer]!.length == 0) {
+        connectionsMap.remove(peer);
+      }
+    };
     conn.addDC(dc, chunkManager.onMessage);
     addPC(peer, connectionID, conn);
     final offer = await pc.createOffer({});
@@ -169,5 +190,55 @@ class WebSocketBasedP2P {
       }
     };
     return c.future;
+  }
+}
+
+class OutgoingMessage {
+  final Uint8List bytes;
+  final Future<void> Function(String error) callback;
+
+  OutgoingMessage(this.bytes, this.callback);
+}
+
+class MaxP2P extends WebSocketBasedP2P {
+  final String peer;
+  final Mutex mutex;
+  BlockingQueue<PeerConnection>? _readyConnections;
+
+  MaxP2P(
+      {required super.deviceID,
+      required this.peer,
+      required super.signaler,
+      required super.onMessage})
+      : mutex = Mutex();
+
+  Future<void> Start(int numConnections) async {
+    _readyConnections = await BlockingQueue<PeerConnection>();
+    final promises = <Future<PeerConnection>>[];
+    for (var idx = 0; idx < numConnections; idx++) {
+      promises.add(super
+          .connect(peer, idx.toString().padLeft(4, '0'))
+          .then((connection) {
+        _readyConnections!.put(connection);
+        return connection;
+      }));
+    }
+    await Future.wait(promises);
+  }
+
+  Future<void> send(final Serialize s) async {
+    final conn = await mutex.protect(() async {
+      return _readyConnections!.get();
+    });
+    await conn.send(s);
+    await mutex.protect(() async {
+      return _readyConnections!.put(conn);
+    });
+    // final conn = connectionsMap.values.first.values.first;
+    // await conn.send(s);
+  }
+
+  Future<void> close() async {
+    await Future.wait(super.connectionsMap[peer]!.values.map((e) => e.close()));
   }
 }
